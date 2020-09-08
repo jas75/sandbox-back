9450e8fb4a56550b1959f8ff08ce8af203b33603
jas75 <azoulayjason@gmail.com>
2020-09-08 11:30:08 +0200

    create route to register user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const authController = require('../controllers/auth-controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// @route   POST /api/user
+// @des     Register/Create a user
+// @access  Public
+router.post('/user', authController.registerUser);
 
 module.exports = router;
