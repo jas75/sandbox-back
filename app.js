6e01a36295a2508b4689d14807eae5ad9cee9770
jas75 <azoulayjason@gmail.com>
2020-09-08 11:16:52 +0200

    Mongoose setup

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +22,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// DB connection
+let mongoUri = config.mongoUri + config.dbName;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+});
+
+connection.on('error', (err) => {
+  console.log(
+    'MongoDb connection error. Please make sure MongoDB is running. ' + err
+  );
+  process.exit();
+});
 
 module.exports = app;
